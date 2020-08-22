06564908626b1c249e8ceb0d7b9c827a469b3b32
Robert Derbyshire <dev.derbyshire@gmail.com>
2020-08-22 10:14:06 +0100

    Added Player component

---
+++ client/src/Player.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Heading } from 'grommet';
+import { Card } from 'react-casino';
+
+function Player ({ name, chips, cards, isDealer, isAdmin, isCurrentPlayer, isMe }) {
+    return (
+        <Box margin="small" round={true} align="center" border={isCurrentPlayer}>
+            <Heading level={3} color={isMe ? 'brand' : null}> {name} ({chips} chips)</Heading>
+            <Box direction="row">
+                {cards.map((card, index) => {
+                    const [value, suit] = [...card];
+
+                    return (
+                        <Card
+                            key={index}
+                            face={value}
+                            suit={suit}
+                            style={{
+                                width: undefined,
+                                height: '12vh',
+                            }}
+                        />
+                    );
+                })}
+            </Box>
+        </Box>
+    );
+}
+
+export default Player
