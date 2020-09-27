047d97c768a1853c141a8f3b8bc5e126193fcce9
Robert Derbyshire <rderbyshire96@gmail.com>
2020-09-27 20:25:22 +0100

    Updated layout of user card Added hover for cards Added indicator for own cards that are not visible

--- client/src/Player.jsx
+++ client/src/Player.jsx
@@ -1,28 +1,88 @@
-import React from 'react';
-import { Box, Heading } from 'grommet';
-import { Card } from 'react-casino';
+import React, { useRef, useState, useEffect } from 'react';
+import { Box, Text } from 'grommet';
+import Casino from 'react-casino';
 
-function Player ({ name, chips, cards, isDealer, isAdmin, isCurrentPlayer, isMe }) {
+const useContainerDimensions = myRef => {
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
+
+    useEffect(() => {
+        const getDimensions = () => ({
+            width: myRef.current.offsetWidth,
+            height: myRef.current.offsetHeight
+        })
+
+        const handleResize = () => {
+            setDimensions(getDimensions())
+        }
+
+        if (myRef.current) handleResize()
+
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [myRef]);
+
+    return dimensions;
+};
+
+function PokerCard ({ face, suit, invisibleToOthers = false, availableDimensions, index }) {
     return (
-        <Box margin="small" round={true} align="center" border={isCurrentPlayer}>
-            <Heading level={3} color={isMe ? 'brand' : null}> {name} ({chips} chips)</Heading>
-            <Box direction="row">
-                {cards.map((card, index) => {
-                    const [value, suit] = [...card];
+        <Box key={index} title={face !== '?' ? `${face}${suit}` : 'Hidden'}>
+            <Casino.Card
+                face={face}
+                suit={suit}
+                style={{
+                    maxWidth: availableDimensions.width / 4.75,
+                    maxHeight: availableDimensions.height,
+                    height: undefined,
+                    width: undefined,
+                    marginLeft: index ? '4px' : null,
+                    filter: invisibleToOthers ? 'opacity(70%)' : 'none'
+                }}
+            />
+        </Box>
+    );
+}
 
-                    return (
-                        <Card
-                            key={index}
+function Player ({ name, chips, cards, isDealer, isAdmin, isCurrentPlayer, isMe, handDescription }) {
+    const topRef = useRef(null);
+    const lowerRef = useRef(null);
+
+    const topDimensions = useContainerDimensions(topRef);
+    const lowerDimensions = useContainerDimensions(lowerRef);
+
+    return (
+        <Box pad="small" fill >
+            <Box pad="xsmall" round={true} fill elevation={isCurrentPlayer ? 'medium': 'small'} border={isCurrentPlayer ? { color: 'accent-1', size: 'medium' } : { color: 'white', size: 'medium' }}>
+                <Text size="xlarge" color={isMe ? 'brand' : null}>{name} ({chips} chips)</Text>
+                <Text level={3} color='gray'>{handDescription}</Text>
+                <Box margin="xsmall" ref={topRef} direction="row" fill>
+                    {[...cards.slice(0, 2), ...cards.slice(6, 7)].map((card, index) => {
+                        const [value, suit] = [...card];
+
+                        return <PokerCard
+                            index={index}
                             face={value}
                             suit={suit}
-                            style={{
-                                width: undefined,
-                                height: '12vh',
-                                marginLeft: index ? '4px' : null
-                            }}
+                            invisibleToOthers={isMe}
+                            availableDimensions={topDimensions}
                         />
-                    );
-                })}
+                    })}
+                </Box>
+                <Box margin="xsmall" ref={lowerRef} direction="row" fill>
+                    {cards.slice(2, 6).map((card, index) => {
+                        const [value, suit] = [...card];
+
+                        return <PokerCard
+                            index={index}
+                            face={value}
+                            suit={suit}
+                            availableDimensions={lowerDimensions}
+                        />
+                    })}
+                </Box>
             </Box>
         </Box>
     );
