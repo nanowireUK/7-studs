0efc63308ceffa0c1e7a244e3b3e4649680d6d2c
Robert Derbyshire <rderbyshire96@gmail.com>
2020-09-27 20:24:21 +0100

    Refactored action buttons into fragments Added UI components for raising by different amounts

--- client/src/GameActions.jsx
+++ client/src/GameActions.jsx
@@ -1,121 +1,145 @@
 import React, { useState } from 'react';
 
-import { Box, Button } from 'grommet';
+import { Box, Button, RangeInput, TextInput } from 'grommet';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { selectCanDoAction, selectIsAdmin, PlayerActions, raise, check, fold, cover, call, reveal, selectHandInProgress, selectHandCompleted, start, selectHandsBeingRevealed } from './redux/slices/game';
+import {
+    selectCanDoAction, selectIsAdmin,
+    selectHandInProgress, selectHandCompleted, selectHandsBeingRevealed,
+    selectAnte, selectMaxRaise,
+    PlayerActions, raise, start, check, fold, cover, call, reveal, } from './redux/slices/game';
+
+function Raise ({ setIsRaising }) {
+    const canRaise = useSelector(selectCanDoAction(PlayerActions.RAISE));
+
+    const clickRaise = () => setIsRaising(true);
+
+    return <Button primary label="Raise [R]" onClick={clickRaise} disabled={!canRaise} />
+}
+
+function Check () {
+    const dispatch = useDispatch();
+    const canCheck = useSelector(selectCanDoAction(PlayerActions.CHECK));
+
+    const clickCheck = () => dispatch(check());
+
+    return <Button primary label="Check [K]" onClick={clickCheck} disabled={!canCheck} />
+}
+
+function Call () {
+    const dispatch = useDispatch();
+    const canCall = useSelector(selectCanDoAction(PlayerActions.CALL));
+
+    const clickCall = () => dispatch(call());
+
+    return <Button primary label="Call [C]" onClick={clickCall} disabled={!canCall} />
+}
+
+function Cover () {
+    const dispatch = useDispatch();
+    const canCover = useSelector(selectCanDoAction(PlayerActions.COVER));
+
+    const clickCover = () => dispatch(cover());
+
+    return <Button primary label="Cover [X]" onClick={clickCover} disabled={!canCover} />
+}
+
+function Fold () {
+    const dispatch = useDispatch();
+    const canFold = useSelector(selectCanDoAction(PlayerActions.FOLD));
+
+    const clickFold = () => dispatch(fold());
+
+    return <Button label="Fold [F]" onClick={clickFold} disabled={!canFold} />
+}
+
+function RevealHand () {
+    const dispatch = useDispatch();
+    const canReveal = useSelector(selectCanDoAction(PlayerActions.REVEAL));
+
+    const clickReveal = () => dispatch(reveal());
+
+    return <Button primary label="Reveal Hand [S]" onClick={clickReveal} disabled={!canReveal} />
+}
+
+function StartNext () {
+    const dispatch = useDispatch();
+    const clickStartNext = () => dispatch(start());
+
+    return <Button primary label="Start Next Game [Enter]" onClick={clickStartNext} />;
+}
+
 
 function GameActions () {
-    const canRaise = useSelector(selectCanDoAction(PlayerActions.RAISE));
-    const canFold = useSelector(selectCanDoAction(PlayerActions.FOLD));
-    const canCheck = useSelector(selectCanDoAction(PlayerActions.CHECK));
-    const canCall = useSelector(selectCanDoAction(PlayerActions.CALL));
-    const canCover = useSelector(selectCanDoAction(PlayerActions.COVER));
-    const canReveal = useSelector(selectCanDoAction(PlayerActions.REVEAL));
     const handInProgress = useSelector(selectHandInProgress);
     const handsBeingRevealed = useSelector(selectHandsBeingRevealed);
     const handCompleted = useSelector(selectHandCompleted);
     const isAdmin = useSelector(selectIsAdmin);
+    const ante = useSelector(selectAnte);
+    const maxRaise = useSelector(selectMaxRaise);
+
 
     const dispatch = useDispatch();
 
     const [isRaising, setIsRaising] = useState(false);
-    const [raiseAmount, setRaiseAmount] = useState('1');
+    const [raiseAmount, setRaiseAmount] = useState(1);
 
-    const clickRaise = () => {
-        dispatch(raise(raiseAmount));
-    };
-
-    const clickCheck = () => {
-        dispatch(check());
-    };
-
-    const clickCall = () => {
-        dispatch(call());
+    const endRaising = () => {
+        setIsRaising(false);
+        setRaiseAmount(ante);
     }
 
-    const clickCover = () => {
-        dispatch(cover());
-    }
+    const raiseIsValid = raiseAmount >= ante && raiseAmount <= maxRaise;
 
-    const clickFold = () => {
-        dispatch(fold());
-    };
-
-    const clickReveal = () => {
-        dispatch(reveal());
-    }
-
-    const clickStartNext = () => {
-        dispatch(start());
-    }
-    
     if (handCompleted) {
          return (
-             <Box direction="row" gap="xsmall">
-                 <Button
-                     primary
-                     label="Reveal Hand [X]"
-                     onClick={clickReveal}
-                     disabled={!canReveal}
-                 />
-                 {isAdmin ? <Button primary label="Start Next Game [Enter]" onClick={clickStartNext}/> : null}
-             </Box>
+            <Box direction="row" gap="xsmall">
+                <RevealHand />
+                {isAdmin ? <StartNext /> : null}
+            </Box>
          );
     } else if (handInProgress) {
+        if (isRaising) return <Box direction="row" gap="xsmall">
+            <Box direction="column" gap="xsmall">
+                <RangeInput min={ante} max={maxRaise} value={raiseAmount} step={1} onChange={(e) => {
+                    setRaiseAmount(e.target.value)
+                }} />
+                <TextInput plain size="small" placeholder="Raise by" value={raiseAmount} onChange={(e) => {
+                    if (e.target.value.trim() === '') setRaiseAmount('');
+                    if (/^\d+$/.test(e.target.value)) {
+                        const raiseBy = Number.parseInt(e.target.value, 10)
+
+                        if (raiseBy >= ante && raiseBy <= maxRaise) setRaiseAmount(raiseBy)
+                        if (raiseBy > maxRaise) setRaiseAmount(maxRaise);
+                    }
+                }}/>
+            </Box>
+            <Button primary label="Raise [Enter]" onClick={() => {
+                if (raiseIsValid) {
+                  dispatch(raise(raiseAmount.toString()));
+                  endRaising();
+                }
+            }} />
+            <Button label="Cancel [Esc]" onClick={endRaising} />
+        </Box>;
         return (
             <Box direction="row" gap="xsmall">
-                <Button
-                    primary
-                    label="Raise [R]"
-                    onClick={clickRaise}
-                    disabled={!canRaise}
-                />
-                <Button
-                    primary
-                    label="Check [K]"
-                    onClick={clickCheck}
-                    disabled={!canCheck}
-                />
-                <Button
-                    primary
-                    label="Fold [F]"
-                    onClick={clickFold}
-                    disabled={!canFold}
-                />
-                <Button
-                    primary
-                    label="Call [C]"
-                    onClick={clickCall}
-                    disabled={!canCall}
-                />
-                <Button
-                    primary
-                    label="Cover [X]"
-                    onClick={clickCover}
-                    disabled={!canCover}
-                />
+                <Raise {...{ setIsRaising }}/>
+                <Check />
+                <Call />
+                <Cover />
+                <Fold />
             </Box>
         );
     } else if (handsBeingRevealed) {
         return (
              <Box direction="row" gap="xsmall">
-                <Button
-                    primary
-                    label="Reveal Hand [X]"
-                    onClick={clickReveal}
-                    disabled={!canReveal}
-                />                
-                    <Button
-                    primary
-                    label="Fold [F]"
-                    onClick={clickFold}
-                    disabled={!canFold}
-                />
+                 <RevealHand />
+                 <Fold />
             </Box>
         )
      } else return null;
-   
+
 }
 
 export default GameActions;
