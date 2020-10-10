17074586f3166208050d96546db0b59069534ae9
Robert Derbyshire <rderbyshire96@gmail.com>
2020-10-10 18:11:19 +0100

    Fixed messy function

--- client/src/Lobby.jsx
+++ client/src/Lobby.jsx
@@ -5,17 +5,10 @@ import { selectUsername, selectGameId } from './redux/slices/hub';
 import { Text, Box, Button, Heading } from 'grommet';
 
 function ordinal(i) {
-    var j = i % 10,
-        k = i % 100;
-    if (j == 1 && k != 11) {
-        return i + "st";
-    }
-    if (j == 2 && k != 12) {
-        return i + "nd";
-    }
-    if (j == 3 && k != 13) {
-        return i + "rd";
-    }
+    const j = i % 10, k = i % 100;
+    if (j === 1 && k !== 11) return i + "st";
+    if (j === 2 && k !== 12) return i + "nd";
+    if (j === 3 && k !== 13) return i + "rd";
     return i + "th";
 }
 
