58a119ce6eb4f7995802dd69582f61985c54db75
JohnRDerbyshire <john@jrd.org.uk>
2021-01-30 13:36:18 +0000

    Handle last player to leave like any other leaver

--- server/Models/ActionLeave.cs
+++ server/Models/ActionLeave.cs
@@ -52,85 +52,88 @@ namespace SevenStuds.Models
             //    2) Every action that is sent to the server should include the count
             //    3) Before processing an action, the server will check that the counts are the same and reject the action (with an exception) if they are not
 
-            // If this was the last player still connected to the game, remove the game from the system 
-            // and notify the player via an exception that the game is now gone
-            if ( G.Participants.Count == 1 ) {
-                //ServerState.EraseGame(G.GameId);
-                throw new HubException("You were the last player to leave the game so the game has now been deleted");
-            }  
-
             Participant p = G.Participants[PlayerIndex];
             p.HasDisconnected = true;
             G.CountOfLeavers++; 
-            
-            string changeOfAdminMessage = "";
-            if ( p.IsGameAdministrator ) {
-                // Randomly nominate first available active player as administrator
-                foreach ( Participant p1 in G.Participants ) {
-                    if ( p1.HasDisconnected == false ) {
-                        p.IsGameAdministrator = false;
-                        p1.IsGameAdministrator = true;
-                        changeOfAdminMessage = ". " + p1.Name + " is new game administrator";
-                        break;
+
+            if ( G.Participants.Count > 1 ) {
+                // If there are any other players still connected to the game, we will have to allow for the effect of this player leaving
+                
+                string changeOfAdminMessage = "";
+                if ( p.IsGameAdministrator ) {
+                    // Randomly nominate first available active player as administrator
+                    foreach ( Participant p1 in G.Participants ) {
+                        if ( p1.HasDisconnected == false ) {
+                            p.IsGameAdministrator = false;
+                            p1.IsGameAdministrator = true;
+                            changeOfAdminMessage = ". " + p1.Name + " is new game administrator";
+                            break;
+                        }
                     }
                 }
-            }
 
-            if ( G.IndexOfParticipantDealingThisHand == PlayerIndex ) {
-                // Leaving player is currently the dealer.
-                // Don't think we have to do anything here because although they are now out of the game,
-                // they are still notionally the dealer (i.e. hand evaluation starts with the player to their left).
-                // Leaving this code in place though as a reminder to think about this when stepping through the code.
-            }
+                if ( G.IndexOfParticipantDealingThisHand == PlayerIndex ) {
+                    // Leaving player is currently the dealer.
+                    // Don't think we have to do anything here because although they are now out of the game,
+                    // they are still notionally the dealer (i.e. hand evaluation starts with the player to their left).
+                    // Leaving this code in place though as a reminder to think about this when stepping through the code.
+                }
 
-            if ( p.HasBeenActiveInCurrentGame == true && p.StartedHandWithNoFunds == false && p.HasFolded == false ) {
-                // Player was still in the game in some form, so we have to treat this as if they folded
-                p.HasFolded = true;
-                G.Participants[PlayerIndex].LastActionInThisHand = ActionEnum.Fold;
-                G.Participants[PlayerIndex].LastActionAmount = 0;   
-                G.Participants[PlayerIndex].RoundNumberOfLastAction = G._CardsDealtIncludingCurrent;
+                if ( p.HasBeenActiveInCurrentGame == true && p.StartedHandWithNoFunds == false && p.HasFolded == false ) {
+                    // Player was still in the game in some form, so we have to treat this as if they folded
+                    p.HasFolded = true;
+                    G.Participants[PlayerIndex].LastActionInThisHand = ActionEnum.Fold;
+                    G.Participants[PlayerIndex].LastActionAmount = 0;   
+                    G.Participants[PlayerIndex].RoundNumberOfLastAction = G._CardsDealtIncludingCurrent;
 
-                // Player is now bankrupt (their contributions to the pots will remain but any uncommitted funds will be discarded at the end of the hand)
-                G.LeaversLogForGame.Add(new LeavingRecord(
-                    p.Name, 
-                    ( p.TimeOfBankruptcy != DateTimeOffset.MinValue ? p.TimeOfBankruptcy 
-                        : ( p.AllInDateTime != DateTimeOffset.MinValue ? p.AllInDateTime : DateTimeOffset.UtcNow )), 
-                    p.ParticipantSignalRGroupName, 
-                    0, 
-                    true, 
-                    false));
-                if ( G.IndexOfParticipantToTakeNextAction == PlayerIndex ) {
-                    // It was player's turn to move anyway, so implement the fold in the same way as if they had just folded in turn
-                    G.RecordLastEvent(UserName + " has left the game and effectively folded"+ changeOfAdminMessage);
-                    await G.SetNextPlayerToActOrHandleEndOfHand(G.IndexOfParticipantToTakeNextAction, G.LastEvent);   
+                    // Player is now bankrupt (their contributions to the pots will remain but any uncommitted funds will be discarded at the end of the hand)
+                    G.LeaversLogForGame.Add(new LeavingRecord(
+                        p.Name, 
+                        ( p.TimeOfBankruptcy != DateTimeOffset.MinValue ? p.TimeOfBankruptcy 
+                            : ( p.AllInDateTime != DateTimeOffset.MinValue ? p.AllInDateTime : DateTimeOffset.UtcNow )), 
+                        p.ParticipantSignalRGroupName, 
+                        0, 
+                        true, 
+                        false));
+                    if ( G.IndexOfParticipantToTakeNextAction == PlayerIndex ) {
+                        // It was player's turn to move anyway, so implement the fold in the same way as if they had just folded in turn
+                        G.RecordLastEvent(UserName + " has left the game and effectively folded"+ changeOfAdminMessage);
+                        await G.SetNextPlayerToActOrHandleEndOfHand(G.IndexOfParticipantToTakeNextAction, G.LastEvent);   
+                    }
+                    else {
+                        // They are leaving out of turn, so it will remain the turn of the current player unless the player leaving now means there is only one person left in
+                        G.RecordLastEvent(UserName + " has left the game and effectively folded out of turn"+ changeOfAdminMessage);
+                        if ( G.CountOfPlayersLeftInHand() == 1 ) {
+                            // Everyone has folded except one player
+                            G.NextAction = await G.ProcessEndOfHand(G.LastEvent + ", only one player left in, hand ended"); // will also update commentary with hand results
+                            G.AddCommentary(G.NextAction);
+                        }
+                    }
                 }
                 else {
-                    // They are leaving out of turn, so it will remain the turn of the current player unless the player leaving now means there is only one person left in
-                    G.RecordLastEvent(UserName + " has left the game and effectively folded out of turn"+ changeOfAdminMessage);
-                    if ( G.CountOfPlayersLeftInHand() == 1 ) {
-                        // Everyone has folded except one player
-                        G.NextAction = await G.ProcessEndOfHand(G.LastEvent + ", only one player left in, hand ended"); // will also update commentary with hand results
-                        G.AddCommentary(G.NextAction);
-                    }
+                    // Player was already out of the hand so no consequence to game flow (we might even be back in the lobby)
+                    G.LeaversLogForGame.Add(new LeavingRecord(
+                        p.Name,
+                        ( p.TimeOfBankruptcy != DateTimeOffset.MinValue ? p.TimeOfBankruptcy 
+                            : ( p.AllInDateTime != DateTimeOffset.MinValue ? p.AllInDateTime : DateTimeOffset.UtcNow )),                      
+                        p.ParticipantSignalRGroupName, 
+                        p.UncommittedChips, 
+                        p.HasBeenActiveInCurrentGame, 
+                        false));
+                    G.RecordLastEvent(p.Name + " has left the game"+changeOfAdminMessage);
+                }
+
+                if ( G.GameMode == GameModeEnum.LobbyOpen ) {
+                    G.RemoveDisconnectedPlayersFromGameState(); // clear out disconnected players
+                    G.LobbyData = new LobbyData(G); // Update the lobby data
                 }
             }
             else {
-                // Player was already out of the hand so no consequence to game flow (we might even be back in the lobby)
-                G.LeaversLogForGame.Add(new LeavingRecord(
-                    p.Name,
-                    ( p.TimeOfBankruptcy != DateTimeOffset.MinValue ? p.TimeOfBankruptcy 
-                        : ( p.AllInDateTime != DateTimeOffset.MinValue ? p.AllInDateTime : DateTimeOffset.UtcNow )),                      
-                    p.ParticipantSignalRGroupName, 
-                    p.UncommittedChips, 
-                    p.HasBeenActiveInCurrentGame, 
-                    false));
-                G.RecordLastEvent(p.Name + " has left the game"+changeOfAdminMessage);
-            }
-
-            if ( G.GameMode == GameModeEnum.LobbyOpen ) {
-                G.RemoveDisconnectedPlayersFromGameState(); // clear out disconnected players
-                G.LobbyData = new LobbyData(G); // Update the lobby data
-            }
+                // If this was the last player still connected to the game, remove the game from the system 
+                // and notify the player via an exception that the game is now gone                
+                //ServerState.EraseGame(G.GameId);
+                //throw new HubException("You were the last player to leave the game so the game has now been deleted");
+            }  
 
             // Set the response type that will trigger the player's client session to disconnect and everyone else's game state to be updated
             SignalRGroupNameForAdditionalNotifications = p.ParticipantSignalRGroupName;
