50787d1b7eeb9f7273a0fbc5dd88c0b756d93441
Robert Derbyshire <rderbyshire96@gmail.com>
2021-03-03 22:57:32 +0000

    Cleaned up HandRankings

--- client/src/components/information/HandRankings.jsx
+++ client/src/components/information/HandRankings.jsx
@@ -1,131 +1,39 @@
-import React from 'react';
-import { Box, Heading, Table, TableBody, TableCell, TableRow, Text } from "grommet";
+import React, { memo } from 'react';
+import { Box, Heading, Text } from "grommet";
 import PokerCard from '../PokerCard';
 
-export default function HandRankings () {
-    return <Box gap="small">
-        <Heading margin="none" level={2}>Hand Rankings</Heading>
-        <Table>
-            <TableBody>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Straight Flush</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="4" suit="H"/>
-                            <PokerCard face="5" suit="H"/>
-                            <PokerCard face="6" suit="H"/>
-                            <PokerCard face="7" suit="H"/>
-                            <PokerCard face="8" suit="H"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>0.03%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Four of a Kind</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="A" suit="H"/>
-                            <PokerCard face="A" suit="D"/>
-                            <PokerCard face="A" suit="C"/>
-                            <PokerCard face="A" suit="S"/>
-                            <PokerCard face="4" suit="D"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>0.17%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Full House</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="8" suit="H"/>
-                            <PokerCard face="8" suit="D"/>
-                            <PokerCard face="8" suit="C"/>
-                            <PokerCard face="K" suit="H"/>
-                            <PokerCard face="K" suit="S"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>2.60%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Flush</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="10" suit="C"/>
-                            <PokerCard face="4" suit="C"/>
-                            <PokerCard face="Q" suit="C"/>
-                            <PokerCard face="7" suit="C"/>
-                            <PokerCard face="2" suit="C"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>3.03%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Straight</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="7" suit="C"/>
-                            <PokerCard face="8" suit="H"/>
-                            <PokerCard face="9" suit="D"/>
-                            <PokerCard face="10" suit="H"/>
-                            <PokerCard face="J" suit="S"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>4.62%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Three of a Kind</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="Q" suit="H"/>
-                            <PokerCard face="Q" suit="C"/>
-                            <PokerCard face="Q" suit="D"/>
-                            <PokerCard face="5" suit="S"/>
-                            <PokerCard face="A" suit="D"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>4.83%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Two Pair</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="3" suit="H"/>
-                            <PokerCard face="3" suit="D"/>
-                            <PokerCard face="6" suit="C"/>
-                            <PokerCard face="6" suit="H"/>
-                            <PokerCard face="K" suit="S"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>23.5%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>Pair</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="5" suit="H"/>
-                            <PokerCard face="5" suit="S"/>
-                            <PokerCard face="2" suit="C"/>
-                            <PokerCard face="J" suit="C"/>
-                            <PokerCard face="A" suit="D"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>43.8%</Text></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell scope="row" direction="column" gap="small">
-                        <Text>High Card</Text>
-                        <Box direction="row" height="35px" gap="xxsmall">
-                            <PokerCard face="2" suit="D"/>
-                            <PokerCard face="5" suit="S"/>
-                            <PokerCard face="6" suit="S"/>
-                            <PokerCard face="J" suit="H"/>
-                            <PokerCard face="A" suit="C"/>
-                        </Box>
-                    </TableCell>
-                    <TableCell scope="row" direction="column" gap="small"><Text>17.4%</Text></TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
-        <br></br>
+const handRankingsData = [
+    { handName: 'Royal Flush', probability: '0.003', exampleCards: ['TS', 'JS', 'QS', 'KS', 'AS'] },
+    { handName: 'Straight Flush', probability: '0.03', exampleCards: ['4H', '5H', '6H', '7H', '8H'] },
+    { handName: 'Four of a Kind', probability: '0.17', exampleCards: ['AH', 'AD', 'AC', 'AS', '4D'] },
+    { handName: 'Full House', probability: '2.60', exampleCards: ['8H', '8D', '8C', 'KH', 'KS'] },
+    { handName: 'Flush', probability: '3.03', exampleCards: ['TC', '4C', 'QC', '7C', '2C'] },
+    { handName: 'Straight', probability: '4.62', exampleCards: ['7C', '8H', '9D', 'TH', 'JS'] },
+    { handName: 'Three of a Kind', probability: '4.83', exampleCards: ['QH', 'QC', 'QD', '5S', 'AD'] },
+    { handName: 'Two Pair', probability: '23.5', exampleCards: ['3H', '3D', '6C', '6H', 'KS'] },
+    { handName: 'Pair', probability: '43.8', exampleCards: ['5H', '5S', '2C', 'JC', 'AD'] },
+    { handName: 'High Card', probability: '17.4', exampleCards: ['2D', '5S', '6S', 'JH', 'AC'] },
+];
+
+function Ranking ({ handName, probability, exampleCards }) {
+    return <Box direction="column" gap="small">
+        <Box direction="row" justify="between">
+            <Text>{handName}</Text>
+            <Text>{probability}%</Text>
+        </Box>
+        <Box height="35px" gap="xxsmall" direction="row">
+            {exampleCards.map(([face, suit], index) => <PokerCard key={index} face={face} suit={suit} />)}
+        </Box>
     </Box>
 }
+
+function HandRankings () {
+    return <Box gap="small">
+        <Heading margin="none" level={2}>Hand Rankings</Heading>
+        {handRankingsData.map(({ handName, probability, exampleCards }) => (
+            <Ranking key={handName} handName={handName} probability={probability} exampleCards={exampleCards} />
+        ))}
+    </Box>
+}
+
+export default memo(HandRankings);
